e4bb746c06bddb5b90accf67620011346c14ab3b
HaseebMansoor <48217879+HaseebMansoor@users.noreply.github.com>
2024-01-10 17:55:29 +0500

    Add files via upload

--- ZAPTATEST.cy.js
+++ ZAPTATEST.cy.js
@@ -1,37 +1,24 @@
 describe('template spec', () => {
+
+  
   it('passes', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
       // returning false here prevents Cypress from failing the test
-      return false;
+      return false;      
     });
+
     cy.visit('https://www.staysucasa.com/');
+    cy.contains('The Sucasa Standard').click();
+    //Write script that will verify that The Sucasa Standard sections should contains following text.
+    cy.contains('Work From Anywhere');
+    cy.contains('Transparent Pricing');
+    cy.contains('Premium Properties');
+    //Click Find Stays Button
+    cy.contains('Find Stays').click();
 
+    //Verify Page URL should be equal to https://www.staysucasa.com/index?#featured
+    cy.url().should('eq', 'https://www.staysucasa.com/index?#featured');
+
+   
     })
-
-  it('passes',()=>{
-    cy.scrollTo('bottom');
-    cy.wait(5000);
-
-  });
-  
-
-  it('passes',()=>{
-
-  });
-
-
-  it('passes',()=>{
-
-  });
-
-
-  it('passes',()=>{
-
-  });
-
-
-  it('passes',()=>{
-
-  });
-
 })
